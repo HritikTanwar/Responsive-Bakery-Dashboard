9cf9f1acb3d8c07c167e28c809380227af646322
Hritik Tanwar <gunnervern6@gmail.com>
2023-07-19 23:45:21 +0530

    New Project

---
+++ index.js
@@ -0,0 +1,76 @@
+
+const AddReminder = document.getElementById("add-reminder")
+let HiddenReminder = document.querySelector(".hidden-reminder")
+let InputField = document.getElementById("input-filed")
+let AddBtn = document.getElementById("add-btn")
+let ReminderList = document.getElementById("reminder-list")
+const darkMode = document.querySelector('.dark-mode');
+const MSG = document.getElementById("msg")
+let AllContent = document.querySelector(".allContent")
+let ToggleReminder = document.getElementById("toggle")
+let Reminders = document.querySelector(".reminders")
+let ShowHome = document.querySelector(".second")
+const mediaQuery = window.matchMedia('(max-width: 890px)');
+
+
+
+ToggleReminder.addEventListener("click", function(){
+    AllContent.style.display = "none"
+    Reminders.style.display = "block"
+    console.log("Clicked")
+})
+
+
+ShowHome.addEventListener("click", function(){
+    if(mediaQuery.matches){ 
+    AllContent.style.display = "block"
+    Reminders.style.display = "none" 
+    }
+    
+});
+
+
+AddReminder.addEventListener("click", function () {
+    HiddenReminder.style.display = "block"
+})
+
+
+
+AddBtn.addEventListener("click", function () {
+    if (InputField.value.trim() == ""){
+        MSG.style.display = "block"
+    }
+    else{   
+    addingReminder()
+    InputField.value = ""
+    HiddenReminder.style.display = "none"
+    MSG.style.display = "none"
+}
+})
+
+
+// Function to create new Reminder
+function addingReminder() {
+    
+    
+    let newReminder = InputField.value
+    let newList = document.createElement("li")
+    newList.textContent = newReminder
+
+    ReminderList.append(newList)
+
+}
+
+// Function to remove Reminder on Double Click!
+ReminderList.addEventListener("dblclick", function () {
+
+    event.target.remove();
+
+})
+
+
+// Function to toggle Dark/Light Mode.
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span').classList.toggle('active');
+})
